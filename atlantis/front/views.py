ace5345412db0b6646a288a76e674bba5e43394a
Juan Abdala <abdalajc@gmail.com>
2015-08-01 19:17:10 -0300

    final

--- atlantis/front/views.py
+++ atlantis/front/views.py
@@ -3,6 +3,7 @@ from django.template import RequestContext
 from django.forms import ModelForm
 from front.models import Client
 import requests
+import json
 # Create your views here.
 
 
@@ -18,14 +19,25 @@ def calcular(request, *args, **kwargs):
             lat = calc.cleaned_data['lat']
             lon = calc.cleaned_data['lon']
             name = calc.cleaned_data['name']
-            mail = calc.cleaned_data['email']
+            email = calc.cleaned_data['email']
             print lat
             print lon
             url = "http://172.18.7.119:8000/atlantis/default/agua.json?"
             data = url + "latitud=" + str(lat) + "&longitud=" + str(lon)
             r = requests.get(data)
-            print r.json
-            print r.text
+            dic = r.json()
+            print dic
+            zone = ""
+            status = ""
+            print dic["rows"][0]
+
+            if dic["rows"][0]:
+                status = "danger"
+                zone = "A sufrido una inundacion "
+            else:
+                zone = "No a sufrido una inundacion "
+                status = "success"
+
     return render_to_response("report.html", locals(),
                               context_instance=RequestContext(request))
 
